93ca6240ee6c991de75dce46f14dfccb148daede
Ubuntu <ubuntu@ip-172-31-45-71.ap-northeast-1.compute.internal>
2025-09-14 05:00:11 +0000

    [finish]画像投稿機能を作ろう

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -36,6 +36,6 @@ class ListsController < ApplicationController
     private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
